52ec4e60062c66c67cb00a45970ff226fd669e96
SolCat <solene.catella@gmail.com>
2018-12-08 10:06:17 +0100

    Début fichier arbre

--- Arbre.jl
+++ Arbre.jl
@@ -6,3 +6,22 @@ mutable struct ArbreMots
 	    return new(false,0,Dict{Char,ArbreMots}())
     end
 end
+
+function segmenterTexte(texte)
+    arbre = ArbreMots()
+end
+
+function ajouterMotsArbre(arbre, ligne)
+end
+
+# Fonction détectant si un mot est présent ou non
+function verifierMot(mot, texte)
+end
+
+# Fonction calculant la longeur moyenne des mots du texte
+function calculerLongMoyMot(texte)
+end
+
+texteCyrano = "./cyrano.txt"
+textePetitPrince = "./le_petit_prince.txt"
+texteOeuvres = "./oeuvres.txt"
